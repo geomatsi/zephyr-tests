bfc7b6ebe040f64aacab1cae5c3ad637a2431a37
Sergey Matyukevich <geomatsi@gmail.com>
2021-03-08 23:02:47 +0300

    nrf52dk-nfc-tag: basic rx
    Basic Rx procedure w/o sending any responses.
    
    Signed-off-by: Sergey Matyukevich <geomatsi@gmail.com>

--- nrf52dk-nfc-tag/src/main.c
+++ nrf52dk-nfc-tag/src/main.c
@@ -5,6 +5,8 @@
  */
 
 #include <zephyr.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include <sys/printk.h>
 
@@ -61,9 +63,100 @@ struct led_desc leds_list[] = {
 	},
 };
 
+#define NFCT_BUFFER_SIZE	540
+
+static uint8_t frame[NFCT_BUFFER_SIZE] = { 0 };
+static bool rx_pending  = false;
+static bool selected = false;
+
+void frame_rx_start()
+{
+	if (rx_pending) {
+		printk("RX: in progress...\r\n");
+		return;
+	}
+
+	memset(frame, 0, NFCT_BUFFER_SIZE);
+	rx_pending = true;
+
+	nrfx_nfct_data_desc_t data = {
+		.data_size = NFCT_BUFFER_SIZE,
+		.p_data = frame,
+	};
+
+	nrfx_nfct_rx(&data);
+}
+
+void frame_rx_end(const nrfx_nfct_evt_rx_frameend_t *rx_frameend)
+{
+	const uint8_t *data = rx_frameend->rx_data.p_data;
+	size_t size = rx_frameend->rx_data.data_size;
+
+	if (!rx_pending) {
+		//printk("RX: %d bytes frame dropped\r\n", size);
+		return;
+	}
+
+	rx_pending = false;
+
+	if (size == 0) {
+		printk("RX: empty frame ?\r\n");
+		return;
+	}
+
+	printk("RX: command 0x%02x\r\n", data[0]);
+	// TODO: process command
+}
+
+void frame_error(const nrfx_nfct_evt_error_t *error)
+{
+	switch (error->reason) {
+	case NRFX_NFCT_ERROR_FRAMEDELAYTIMEOUT:
+		printk("ERR: frame delay timeout\r\n");
+		break;
+	default:
+		printk("ERR: unknown reason: %d\r\n", error->reason);
+		break;
+	}
+}
+
 void nfct_notifier(nrfx_nfct_evt_t const *e)
 {
-	printk("TAG event: %u\r\n", e->evt_id);
+	switch (e->evt_id) {
+	case NRFX_NFCT_EVT_FIELD_DETECTED:
+		//printk("EVT: field detected\r\n");
+		nrfx_nfct_state_force(NRFX_NFCT_STATE_ACTIVATED);
+		break;
+	case NRFX_NFCT_EVT_FIELD_LOST:
+		//printk("EVT: field lost\r\n");
+		selected = false;
+		break;
+	case NRFX_NFCT_EVT_SELECTED:
+		printk("EVT: tag selected\r\n");
+		selected = true;
+		frame_rx_start();
+		break;
+	case NRFX_NFCT_EVT_RX_FRAMESTART:
+		//printk("EVT: frame rx start\r\n");
+		break;
+	case NRFX_NFCT_EVT_RX_FRAMEEND:
+		//printk("EVT: frame rx end\r\n");
+		frame_rx_end(&e->params.rx_frameend);
+		break;
+	case NRFX_NFCT_EVT_TX_FRAMESTART:
+		printk("EVT: frame tx start\r\n");
+		break;
+	case NRFX_NFCT_EVT_TX_FRAMEEND:
+		printk("EVT: frame tx end\r\n");
+		frame_rx_start();
+		break;
+	case NRFX_NFCT_EVT_ERROR:
+		frame_error(&e->params.error);
+		break;
+	default:
+		printk("EVT: unknown event %u\r\n", e->evt_id);
+		break;
+	}
 }
 
 const nrfx_nfct_config_t nfct_config = {
